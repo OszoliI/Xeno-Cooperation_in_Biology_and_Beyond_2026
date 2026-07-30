1ffbff9fe301977543afbabd2bea78c01984fbbd
OszoliI <160592617+OszoliI@users.noreply.github.com>
2026-07-30 18:33:49 +0200

    Add files via upload

--- javascripts/elecronic mail reveal.js
+++ javascripts/elecronic mail reveal.js
@@ -1,36 +1,60 @@
-function _sysDecode(_str) {
-    const _k = 42; // XOR kulcs
-    let _r = '';
-    for (let _i = 0; _i < _str.length; _i++) {
-        _r += String.fromCharCode(_str.charCodeAt(_i) ^ _k);
+(function(_0x51a2, _0x2c3b) {
+    const _0x4d5e = _0x51a2();
+    while (!![]) {
+        try {
+            const _0x3a2b = parseInt(_0x1f2e(0x1)) / 1 + parseInt(_0x1f2e(0x2)) / 2 + (-parseInt(_0x1f2e(0x3)) / 3) * (parseInt(_0x1f2e(0x4)) / 4) + parseInt(_0x1f2e(0x5)) / 5 + -parseInt(_0x1f2e(0x6)) / 6 + parseInt(_0x1f2e(0x7)) / 7 + -parseInt(_0x1f2e(0x8)) / 8;
+            if (_0x3a2b === _0x2c3b) break; else _0x4d5e['push'](_0x4d5e['shift']());
+        } catch (_0x6f1a) {
+            _0x4d5e['push'](_0x4d5e['shift']());
+        }
     }
-    return _r;
+}(_0x1122, 0x3d0b5));
+
+function _0x1122() {
+    const _0x8899 = ['4645236XvIqcG', '1251944yOQZfI', '7651048bXzVvF', '1406852VvDsyj', '174GvXwIe', '38450fOQpZE', '241160DOfFrc', '11831888bYwKQU', 'querySelectorAll', 'bWhoY2N6YmNreG93WmhreWJidW9MbXV0', 'YGttaW13TGltbWttaUptYm9idXQ=', 'innerHTML', '<a\x20href=\x22mailto:', '\x22>', '</a>', '.captcha-wrapper', 'style', 'display', 'none'];
+    _0x1122 = function() { return _0x8899; };
+    return _0x1122();
 }
 
-function initializeSecureData(token) {
-    const _targetClass = ['.', 'secure', '-', 'email', '-', 'cell'].join('');
-    const cells = document.querySelectorAll(_targetClass);
+function _0x1f2e(_0x1122_inner) {
+    const _0x1122_alias = _0x1122();
+    return _0x1f2e = function(_0x1f2e_inner) {
+        _0x1f2e_inner = _0x1f2e_inner - 0x1;
+        return _0x1122_alias[_0x1f2e_inner];
+    }, _0x1f2e(_0x1122_inner);
+}
+
+function _fastXOR(_0xstr) {
+    const _key = 42;
+    let _res = '';
+    for (let _i = 0; _i < _0xstr.length; _i++) {
+        _res += String.fromCharCode(_0xstr.charCodeAt(_i) ^ _key);
+    }
+    return _res;
+}
+
+function initializeSecureData(_0xtoken) {
+    const _0xsel1 = _fastXOR(window['atob'](_0x1f2e(0xa)));
+    const _0xsel2 = _fastXOR(window['atob'](_0x1f2e(0xb)));
     
-    cells.forEach(cell => {
-        // Dinamikusan olvassuk ki az attribútumot is
-        const _attrName = ['data', '-', 'email'].join('');
-        const encodedData = cell.getAttribute(_attrName);
-        
-        if (encodedData) {
+    const _0xcells = document[_0x1f2e(0x9)](_0xsel1);
+    
+    _0xcells['forEach'](function(_0xcell) {
+        const _0xdata = _0xcell['getAttribute'](_0xsel2);
+        if (_0xdata) {
             try {
-                const stage1 = window.atob(encodedData.trim());
-                const realEmail = _sysDecode(stage1);
+                const _0xbase64Decoded = window['atob'](String(_0xdata)['trim']());
+                const _0xresolvedNode = _fastXOR(_0xbase64Decoded);
                 
-                cell.innerHTML = '<a href="mailto:' + realEmail + '">' + realEmail + '</a>';
-            } catch (e) {
-                console.error("Decoding issue:", e);
-                cell.innerHTML = 'Error';
+                _0xcell[_0x1f2e(0xc)] = _0x1f2e(0xd) + _0xresolvedNode + _0x1f2e(0xe) + _0xresolvedNode + _0x1f2e(0xf);
+            } catch (_0xerr) {
+                _0xcell[_0x1f2e(0xc)] = 'Error';
             }
         }
     });
     
-    const wrapper = document.querySelector('.captcha-wrapper');
-    if (wrapper) {
-        wrapper.style.display = 'none';
+    const _0xwrap = document['querySelector'](_0x1f2e(0x10));
+    if (_0xwrap) {
+        _0xwrap[_0x1f2e(0x11)][_0x1f2e(0x12)] = _0x1f2e(0x13);
     }
 }
