2c3ab602a3486d580bc30539d95efa8c2400490a
OszoliI <160592617+OszoliI@users.noreply.github.com>
2026-09-02 21:24:07 +0200

    add author list to abstracts, more personal information to organisers, topics to About

--- javascripts/data reveal.js
+++ javascripts/data reveal.js
@@ -1,85 +1,67 @@
-const _0x9a2b=['\x64\x61\x74\x61','\x74\x72\x69\x6d','\x61\x74\x6f\x62','\x71\x75\x65\x72\x79\x53\x65\x6c\x65\x63\x74\x6f\x72\x41\x6c\x6c','\x2e\x73\x65\x63\x75\x72\x65\x2d\x63\x65\x6c\x6c','\x66\x6f\x72\x45\x61\x63\x68','\x67\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65','\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c','\x3c\x61\x20\x68\x72\x65\x66\x3d\x22\x6d\x61\x69\x6c\x74\x6f\x3a','\x22\x3e','\x3c\x2f\x61\x3e','\x45\x72\x72\x6f\x72','\x71\x75\x65\x72\x79\x53\x65\x6c\x65\x63\x74\x6f\x72','\x2e\x63\x61\x70\x74\x63\x68\x61\x2d\x77\x72\x61\x70\x70\x65\x72','\x73\x74\x79\x6c\x65','\x64\x69\x73\x70\x6c\x61\x79','\x6e\x6f\x6e\x65','\x6c\x65\x6e\x67\x74\x68','\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65','\x63\x68\x61\x72\x43\x6f\x64\x65\x41\x74'];
+(function(_0xglobal) {
+    var _0x9a2b = [
+        '\x71\x75\x65\x72\x79\x53\x65\x6c\x65\x63\x74\x6f\x72\x41\x6c\x6c', 
+        '\x2e\x73\x65\x63\x75\x72\x65\x2d\x63\x65\x6c\x6c', 
+        '\x2e\x73\x65\x63\x75\x72\x65\x2d\x63\x65\x6c\x6c\x2d\x32', 
+        '\x67\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65', 
+        '\x64\x61\x74\x61', 
+        '\x61\x74\x6f\x62', 
+        '\x74\x72\x69\x6d', 
+        '\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c', 
+        '\x3c\x61\x20\x68\x72\x65\x66\x3d\x22\x6d\x61\x69\x6c\x74\x6f\x3a', 
+        '\x22\x3e', 
+        '\x3c\x2f\x61\x3e', 
+        '\x45\x72\x72\x6f\x72', 
+        '\x3c\x69\x6d\x67\x20\x73\x72\x63\x3d\x22', 
+        '\x71\x75\x65\x72\x79\x53\x65\x6c\x65\x63\x74\x6f\x72', 
+        '\x2e\x63\x61\x70\x74\x63\x68\x61\x2d\x77\x72\x61\x70\x70\x65\x72', 
+        '\x73\x74\x79\x6c\x65', 
+        '\x64\x69\x73\x70\x6c\x61\x79', 
+        '\x6e\x6f\x6e\x65', 
+        '\x6c\x65\x6e\x67\x74\x68', 
+        '\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65', 
+        '\x63\x68\x61\x72\x43\x6f\x64\x65\x41\x74', 
+        '\x69\x6e\x69\x74\x69\x61\x6c\x69\x7a\x65\x53\x65\x63\x75\x72\x65\x44\x61\x74\x61' 
+    ];
 
-function _0x3b8d(_0x1f2){
-    let _0x4d5 = (0x54 >> 1);
-    let _0x5e6 = '';
-    let _0xidx = (0x1 >> 1);
-    
-    while (_0xidx < _0x1f2[_0x9a2b[(0x22 >> 1)]]) { 
-        let _0xchar = _0x1f2[_0x9a2b[(0x26 >> 1)]](_0xidx);
-        
-        let _0x7b3 = (_0xchar | _0x4d5) & ~(_0xchar & _0x4d5);
-        
-        if (((_0xidx << 3) >> 3) !== _0xidx) {
-            _0x5e6 += String[_0x9a2b[(0x24 >> 1)]]((0x4 << 2) | _0x7b3);
-        } else {
-            _0x5e6 += String[_0x9a2b[(0x24 >> 1)]](_0x7b3);
+    var _0x5f1a = function(_0x1d4e) {
+        var _0x7c9b = [];
+        var _0x3e12 = (0x15 << 1); 
+        for (var _0x2f4c = 0; _0x2f4c < _0x1d4e[_0x9a2b[(0x24 >> 1)]]; _0x2f4c++) {
+            var _0xchar = _0x1d4e[_0x9a2b[(0x28 >> 1)]](_0x2f4c);
+            _0x7c9b['\x70\x75\x73\x68'](String[_0x9a2b[(0x26 >> 1)]](_0xchar ^ _0x3e12));
         }
-        _0xidx = _0xidx + 1;
-    }
-    return _0x5e6;
-}
+        return _0x7c9b['\x6a\x6f\x69\x6e']('');
+    };
 
-function initializeSecureData(_0x7a8){
-    let _0xstate = (0x1 >> 1); 
-    var _0x8b9, _0xe1f;
-
-    while(_0xstate !== (0x10 >> 1)) {
-        switch(_0xstate) {
-            case (0x1 >> 1): 
-                _0x8b9 = document[_0x9a2b[(0x6 >> 1)]](_0x9a2b[(0x8 >> 1)]);
-                _0xstate = (0x3 >> 1);
-                break;
-                
-            case (0x3 >> 1): 
-                if (_0x8b9) {
-                    _0xstate = (0x4 >> 1);
-                } else {
-                    _0xstate = (0x10 >> 1);
+    var _0x3f2c = function(_0xsel, _0xrenderFn) {
+        var _0xels = document[_0x9a2b[(0x0)]](_0xsel);
+        for (var _0xidx = 0; _0xidx < _0xels[_0x9a2b[(0x24 >> 1)]]; _0xidx++) {
+            var _0xel = _0xels[_0xidx];
+            var _0xdata = _0xel[_0x9a2b[(0x6 >> 1)]](_0x9a2b[(0x8 >> 1)]);
+            if (_0xdata) {
+                try {
+                    var _0xdecoded = _0x5f1a(_0xglobal[_0x9a2b[(0xA >> 1)]](_0xdata[_0x9a2b[(0xC >> 1)]]()));
+                    _0xel[_0x9a2b[(0xE >> 1)]] = _0xrenderFn(_0xdecoded);
+                } catch (_0xe) {
+                    _0xel[_0x9a2b[(0xE >> 1)]] = _0x9a2b[(0x16 >> 1)];
                 }
-                break;
-                
-            case (0x4 >> 1): 
-                _0x8b9[_0x9a2b[(0xA >> 1)]](_0x9ca => {
-                    let _0xinnerState = (0x1 >> 1);
-                    var _0xadb;
-                    
-                    while(_0xinnerState !== (0x8 >> 1)) { 
-                        if (_0xinnerState === (0x1 >> 1)) {
-                            _0xadb = _0x9ca[_0x9a2b[(0xC >> 1)]](_0x9a2b[(0x1 >> 1)]);
-                            _0xinnerState = (0x3 >> 1);
-                        } else if (_0xinnerState === (0x3 >> 1)) {
-                            if (_0xadb) { 
-                                _0xinnerState = (0x4 >> 1); 
-                            } else { 
-                                _0xinnerState = (0x8 >> 1); 
-                            }
-                        } else if (_0xinnerState === (0x4 >> 1)) {
-                            try {
-                                let _0xbec = window[_0x9a2b[(0x4 >> 1)]](_0xadb[_0x9a2b[(0x3 >> 1)]]());
-                                let _0xcfd = _0x3b8d(_0xbec);
-                                _0x9ca[_0x9a2b[(0xE >> 1)]] = _0x9a2b[(0x10 >> 1)] + _0xcfd + _0x9a2b[(0x12 >> 1)] + _0xcfd + _0x9a2b[(0x14 >> 1)];
-                            } catch(_0xd0e) {
-                                _0x9ca[_0x9a2b[(0xE >> 1)]] = _0x9a2b[(0x16 >> 1)];
-                            }
-                            _0xinnerState = (0x8 >> 1); 
-                        }
-                    }
-                });
-                _0xstate = (0x6 >> 1);
-                break;
-                
-            case (0x6 >> 1): 
-                _0xe1f = document[_0x9a2b[(0x18 >> 1)]](_0x9a2b[(0x1A >> 1)]);
-                _0xstate = (0x8 >> 1);
-                break;
-                
-            case (0x8 >> 1): 
-                if (_0xe1f) {
-                    _0xe1f[_0x9a2b[(0x1C >> 1)]][_0x9a2b[(0x1E >> 1)]] = _0x9a2b[(0x20 >> 1)];
-                }
-                _0xstate = (0x10 >> 1);
-                break;
+            }
         }
-    }
-}
+    };
+
+    _0xglobal[_0x9a2b[(0x2A >> 1)]] = function() {
+        _0x3f2c(_0x9a2b[(0x2 >> 1)], function(_0xdec) {
+            return _0x9a2b[(0x10 >> 1)] + _0xdec + _0x9a2b[(0x12 >> 1)] + _0xdec + _0x9a2b[(0x14 >> 1)];
+        });
+        
+        _0x3f2c(_0x9a2b[(0x4 >> 1)], function(_0xdec) {
+            return _0x9a2b[(0x18 >> 1)] + _0xdec + _0x9a2b[(0x12 >> 1)];
+        });
+
+        var _0xwrap = document[_0x9a2b[(0x1A >> 1)]](_0x9a2b[(0x1C >> 1)]);
+        if (_0xwrap) {
+            _0xwrap[_0x9a2b[(0x1E >> 1)]][_0x9a2b[(0x20 >> 1)]] = _0x9a2b[(0x22 >> 1)];
+        }
+    };
+})(window);
